e5f443ee28d0dc9278d1835007fb8f3ae9e9f65d
Guido Schmidt <guido.schmidt.2912@gmail.com>
2013-10-18 23:14:11 +0200

    CMake cross-build: unix and windows

--- src/Tone.cpp
+++ src/Tone.cpp
@@ -1,10 +1,16 @@
 #include "./renderer/Context.h"
+#include "./utils/Common.h"
 
 int main(void)
 {
 	tone::renderer::Context* context = new tone::renderer::Context( glm::ivec2(1024, 768) );
-	while(true)
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(1, 1, 1, 1);
+
+    while(context->isLive())
 	{
+
 		context->swapBuffers();
 	}
 
