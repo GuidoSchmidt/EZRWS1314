79f232b9f40b4ff6d27a47c0e6742f8a7e6d0451
GuidoSchmidt <guido.schmidt.2912@gmail.com>
2013-10-18 21:29:27 +0200

    bin folder not under versioncontrol anymore

--- src/Tone.cpp
+++ src/Tone.cpp
@@ -5,11 +5,11 @@ int main(void)
 {
 	tone::renderer::Context* context = new tone::renderer::Context( glm::ivec2(1024, 768) );
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(1, 1, 1, 1);
 
     while(context->isLive())
 	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		context->swapBuffers();
 	}
