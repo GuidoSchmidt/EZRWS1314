d7694949c287f149d836c85989d8dd231abeb7b9
GuidoSchmidt <guido.schmidt.2912@gmail.com>
2013-12-10 15:33:07 +0100

    added resource folder

--- src/utils/FullscreenTriangle.h
+++ src/utils/FullscreenTriangle.h
@@ -3,13 +3,30 @@
 #ifndef H_FULLSCRNTRIANGLE
 #define	H_FULLSCRNTRIANGLE
 
+#include "../utils/Common.h"
+
 namespace utils
 {
 	class FullscreenTriangle
 	{
-		public:
+		private:
+			GLuint VAO_handle;
+			GLuint VBO_handle, IBO_handle, NBO_handle, UVBO_handle;
+
+			//! \brief Creates the geometry buffers for drawing a screen filling triangle
+			void createGeometry(void);
+
+			//! \brief Constructor: private because this class is singleton
 			FullscreenTriangle();
+
+			//! \brief Destructor
 			~FullscreenTriangle();
+		
+		public:
+			//! \brief Returns the singleton instance
+			static FullscreenTriangle* instance(void);
+
+			static void draw(void);
 	};
 }
 
