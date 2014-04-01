c27f532fc39d0be385a743eecdf9bf885e2b6810
markus <solbach.m@gmail.com>
2014-04-01 19:01:55 +0200

    new function: readability...even mooooore

--- src/renderer/Renderer.h
+++ src/renderer/Renderer.h
@@ -21,75 +21,75 @@
 #include "../utils/Importer.h"
 #include "../utils/FullscreenTriangle.h"
 
-namespace renderer {		
-	//! @class Renderer
-	class Renderer
-	{
-		private:
-			unsigned int m_framecount;
-			int m_fps;
-			double m_current_time, m_previous_time;
+namespace renderer {
+//! @class Renderer
+class Renderer
+{
+private:
+    unsigned int m_framecount;
+    int m_fps;
+    double m_current_time, m_previous_time;
 
-			Context* m_context;
-            //! Shader programs
-            ShaderProgram* m_shaderProgram_simple;
-            ShaderProgram* m_shaderProgram_compositing;
-            //! Framebuffer objects
-            FrameBufferObject* m_fbo;
+    Context* m_context;
+    //! Shader programs
+    ShaderProgram* m_shaderProgram_simple;
+    ShaderProgram* m_shaderProgram_compositing;
+    //! Framebuffer objects
+    FrameBufferObject* m_fbo;
 
-            //! Scene
-            utils::FullscreenTriangle* m_fullscreen_triangle;
-			scene::Camera* m_scene_camera;
-			std::vector<scene::Geometry*> m_renderqueue;
+    //! Scene
+    utils::FullscreenTriangle* m_fullscreen_triangle;
+    scene::Camera* m_scene_camera;
+    std::vector<scene::Geometry*> m_renderqueue;
 
-			SlimQuad* fsq;
-			SlimFBO* gBuffer;
-			SlimFBO* lightingFBO;
-			SlimFBO* sunlightFBO0;
-			SlimFBO* sunlightFBO1;
-			SlimFBO* sunlightFBO2;
-			SlimFBO* sunlightFBO3;
-			SlimFBO* sunlightFBO4;
+    SlimQuad* fsq;
+    SlimFBO* gBuffer;
+    SlimFBO* lightingFBO;
+    SlimFBO* sunlightFBO0;
+    SlimFBO* sunlightFBO1;
+    SlimFBO* sunlightFBO2;
+    SlimFBO* sunlightFBO3;
+    SlimFBO* sunlightFBO4;
 
-			//scene::SceneNode* sun;
-			glm::vec4 wsSunPos;
-			glm::vec4 ssSunPos;
-			SeparatedBlurPass* blurPass;
-			RadialGlowMaskPass* maskPass;
-			RadialLuminancePass* luminancePass;
-			FinalPass* finalPass;
-			//PhongPass* phong1;
-			//GlowPass* glowHalf;
+    //scene::SceneNode* sun;
+    glm::vec4 wsSunPos;
+    glm::vec4 ssSunPos;
+    SeparatedBlurPass* blurPass;
+    RadialGlowMaskPass* maskPass;
+    RadialLuminancePass* luminancePass;
+    FinalPass* finalPass;
+    //PhongPass* phong1;
+    //GlowPass* glowHalf;
 
-			//Rocket::Core::Context* context;
-			//Shell* shell;
+    //Rocket::Core::Context* context;
+    //Shell* shell;
 
-			//! \brief Constructor (singleton)
-			Renderer(void);
+    //! \brief Constructor (singleton)
+    Renderer(void);
 
-			//! \brief Destructor (singleton)
-			~Renderer(void);
+    //! \brief Destructor (singleton)
+    ~Renderer(void);
 
-			//! \brief Initializes the renderer
-			void init(void);
+    //! \brief Initializes the renderer
+    void init(void);
 
-			//! \brief Sets needed OpenGL states
-			void setupGL(void);
+    //! \brief Sets needed OpenGL states
+    void setupGL(void);
 
-                        //! \brief Sets the needes shader stages and fbos
-                        void setupShaderStages(void);
-				
-		public:
-			//! Returns the singleton instance
-			static Renderer* instance(Context& context);
+    //! \brief Sets the needes shader stages and fbos
+    void setupShaderStages(void);
 
-			//! \brief Sets the context to render to
-			void setRenderContext(Context& context);
+public:
+    //! Returns the singleton instance
+    static Renderer* instance(Context& context);
 
-			//! \brief Calls the render loop
-			void renderloop(void);
+    //! \brief Sets the context to render to
+    void setRenderContext(Context& context);
 
-			void doTheSunlightEffect(void);
-	};
+    //! \brief Calls the render loop
+    void renderloop(void);
+
+    void doTheSunlightEffect(void);
+};
 }
 #endif //H_RENDERER
